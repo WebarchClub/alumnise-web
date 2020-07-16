14b5ad4a475257c1611f998c163dd223105af320
ampsteric <ampsteric@gmail.com>
2020-07-16 22:33:18 +0530

    added react form

--- server/index.js
+++ server/index.js
@@ -1,64 +1,69 @@
-require('dotenv').config();
+require("dotenv").config();
 
 //NOTE : USE NPM RUN DEV INSTEAD OF NPM RUN START
 
-
-const express = require('express');
-const cors = require('cors');
-const passport = require('passport');
-const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
-const keys = require('../config')
-const chalk = require('chalk')
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
+const keys = require("../config");
+const chalk = require("chalk");
 let user = {};
 
-passport.serializeUser((user,done) => {
-    done(null,user);
-})
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
 
-passport.deserializeUser((user,done) => {
-    done(null,user);
-})
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
 
 var mailId;
-passport.use(new LinkedInStrategy({
-  clientID: keys.LINKEDIN.clientID,
-  clientSecret: keys.LINKEDIN.clientSecret,
-	 callbackURL: 'http://localhost:3000/auth/linkedin/callback',
-  scope: ['r_emailaddress', 'r_liteprofile'],
-}, function(accessToken, refreshToken, profile, done) {
-      console.log(chalk.cyan(JSON.stringify(profile))); // all the details of the user
-      profile.emails.forEach(email => {   
-				mailId = email.value;
-      })
-      console.log(chalk.green(mailId)); //mail id
-      user = {...profile};
-      return done(null,profile);
-}));
+passport.use(
+    new LinkedInStrategy(
+        {
+            clientID: keys.LINKEDIN.clientID,
+            clientSecret: keys.LINKEDIN.clientSecret,
+            callbackURL: "http://localhost:3000/auth/linkedin/callback",
+            scope: ["r_emailaddress", "r_liteprofile"],
+        },
+        function (accessToken, refreshToken, profile, done) {
+            // console.log(chalk.cyan(JSON.stringify(profile))); // all the details of the user
+            // profile.emails.forEach((email) => {
+            //     mailId = email.value;
+            // });
+            // console.log(profile);
+            // console.log(chalk.green(mailId)); //mail id
+            user = { ...profile };
+            return done(null, profile);
+        }
+    )
+);
 
+const app = express();
+app.use(cors());
+app.use(passport.initialize());
 
-  const app = express();
-  app.use(cors());
-  app.use(passport.initialize());
+app.get("/auth/linkedin", passport.authenticate("linkedin"));
+app.get(
+    "/auth/linkedin/callback",
+    passport.authenticate("linkedin"),
+    (req, res) => {
+        res.redirect("/");
+    }
+);
 
-  app.get('/auth/linkedin',passport.authenticate('linkedin'));
-  app.get('/auth/linkedin/callback',passport.authenticate('linkedin'),
-                    (req,res) => 
-                    {
-                            res.redirect('/');
-                    });
-
-
-app.get("/user",(req,res) => {
-    console.log('getting user data');
+app.get("/user", (req, res) => {
+    console.log("getting user data");
     res.send(user);
-})
+});
 
-app.get('/auth/logout',(req,res) => {
-    console.log('logging out');
-     user = {};
-    res.redirect('/');
-})
+app.get("/auth/logout", (req, res) => {
+    console.log("logging out");
+    user = {};
+    res.redirect("/");
+});
 
-  const PORT = 5000;
+const PORT = 5000;
 
-  app.listen(PORT);
+app.listen(PORT);
--- src/components/displays/Terminal.js
+++ src/components/displays/Terminal.js
@@ -1,18 +1,137 @@
-import React from 'react'
+import React from "react";
+import firebase from "./firebase";
+// import Example from "./modal/modal";
+import "./App.css";
 
-const Terminal = ({userData}) => {
+const Terminal = ({ userData }) => {
+    const [name, setname] = React.useState("");
+    const [batch, setbatch] = React.useState("");
+    const [company, setcompany] = React.useState("");
+    const [position, setposition] = React.useState("");
+    const [salary, setsalary] = React.useState("");
+    const [description, setdescription] = React.useState("");
+    const [placed, setplaced] = React.useState("");
+    const [require, setrequire] = React.useState("");
     const userID = userData.id;
-    const name = userData.displayName;
-    return(
-        <div className="window">
-        <div className="title-bar">
-        <div className="content">
-            <pre>{userID}</pre>
-            <pre>{name}</pre>
-        </div>
-        </div>
-        </div>
-    )
-}
+    const Username = userData.displayName;
 
- export default Terminal;
+    const addDataHandler = (e) => {
+        e.preventDefault();
+
+        const db = firebase.firestore();
+        db.collection("placement")
+            .add({
+                approve: false,
+                name: name,
+                batch: batch,
+                company: company,
+                position: position,
+                salary: salary,
+                description: description,
+                placed: true,
+                requirements: require,
+            })
+            .then(function () {
+                console.log("Document successfully written!");
+            })
+            .catch(function (error) {
+                console.error("Error writing document: ", error);
+            });
+    };
+    const handleOptionChange = (e) => {
+        // placed: changeEvent.target.value
+        setplaced(e.target.value);
+    };
+    return (
+        <div className="window">
+            <div className="title-bar">
+                <div className="content">
+                    <h1>Registration Form</h1>
+
+                    <div className="content">
+                        <form id="add-cafe-form">
+                            <input
+                                id="forminput"
+                                type="text"
+                                name="name"
+                                value={Username}
+                                placeholder="Name"
+                                onChange={(e) => setname(e.target.value)}
+                            />
+                            <input
+                                id="forminput"
+                                type="text"
+                                name="batch"
+                                placeholder="Batch"
+                                onChange={(e) => setbatch(e.target.value)}
+                            />
+                            <input
+                                id="forminput"
+                                type="text"
+                                name="company"
+                                placeholder="Company"
+                                onChange={(e) => setcompany(e.target.value)}
+                            />
+                            <input
+                                id="forminput"
+                                type="text"
+                                name="require"
+                                placeholder="Please mention the requirements.."
+                                onChange={(e) => setrequire(e.target.value)}
+                            />
+                            <input
+                                id="forminput"
+                                type="text"
+                                name="position"
+                                placeholder="Position"
+                                onChange={(e) => setposition(e.target.value)}
+                            />
+                            <input
+                                id="forminput"
+                                type="text"
+                                name="salary"
+                                placeholder="Salary"
+                                onChange={(e) => setsalary(e.target.value)}
+                            />
+                            <p id="question">
+                                Were You selected for the Interviews?
+                            </p>
+                            <label id="radiolabel">
+                                <input
+                                    type="radio"
+                                    value="Yes"
+                                    checked={placed === "Yes"}
+                                    onChange={handleOptionChange}
+                                />
+                                Yes
+                            </label>
+
+                            <label id="radiolabel">
+                                <input
+                                    type="radio"
+                                    value="No"
+                                    checked={placed === "No"}
+                                    onChange={handleOptionChange}
+                                />
+                                No
+                            </label>
+
+                            <textarea
+                                name="description"
+                                placeholder="Description"
+                                id="forminput"
+                                onChange={(e) => setdescription(e.target.value)}
+                            ></textarea>
+                            <button id="myBtn" onClick={addDataHandler}>
+                                Submit
+                            </button>
+                            {/* <Example /> */}
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Terminal;
